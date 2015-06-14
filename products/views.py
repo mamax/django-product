a6468162f6d8d3bce12ecbb15723f57588f6d109
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-06-14 22:34:19 +0300

    dynamic list of products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,8 +7,26 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'products/index.html', {})
 
+
 def products_list(request):
-    return render(request, 'products/products_list.html', {})
+    products = (
+        {'id': 1,
+         'name': u'Bread',
+         'slug': u'Black',
+         'description': u'for eating',
+         'price': 235,
+         'created_at': 2.12,
+         'modified_at': 22.12},
+        {'id': 2,
+         'name': u'Meat',
+         'slug': u'Red',
+         'description': u'for eating',
+         'price': 234,
+         'created_at': 12.09,
+         'modified_at': 13.09},
+    )
+    return render(request, 'products/products_list.html', {'products': products})
+
 
 def products_add(request):
     return HttpResponse('<h1>Add Product</h1>')
