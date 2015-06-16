e852a2030ddd219321cc634d90edfaa697b5200e
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-06-16 16:43:12 +0300

    add initial models to migration

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -14,16 +14,16 @@ class Migration(migrations.Migration):
             name='Product',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('first_name', models.CharField(max_length=256, verbose_name='\u0406\u043c\u044f')),
-                ('slug', models.SlugField(max_length=50, verbose_name='\u041f\u0440\u0456\u0437\u0432\u0438\u0449\u0435')),
-                ('description', models.CharField(max_length=256, verbose_name='\u041f\u0440\u0456\u0437\u0432\u0438\u0449\u0435')),
-                ('price', models.DecimalField(max_digits=5, decimal_places=2, verbose_name='\u0411\u0456\u043b\u0435\u0442')),
-                ('created_at', models.DateField(verbose_name='\u0414\u0430\u0442\u0430\u043d\u0430\u0440\u043e\u0434\u0436\u0435\u043d\u043d\u044f')),
-                ('modified_at', models.DateField(verbose_name='\u0414\u0430\u0442\u0430\u043d\u0430\u0440\u043e\u0434\u0436\u0435\u043d\u043d\u044f', blank=True)),
+                ('first_name', models.CharField(max_length=256, verbose_name='\u041d\u0430\u0437\u0432\u0430')),
+                ('slug', models.SlugField(verbose_name='\u041a\u043e\u0440\u043e\u0442\u043a\u0430 \u043d\u0430\u0437\u0432\u0430-\u043c\u0456\u0442\u043a\u0430')),
+                ('description', models.CharField(max_length=256, verbose_name='\u041e\u043f\u0438\u0441')),
+                ('price', models.FloatField(verbose_name='\u0426\u0456\u043d\u0430')),
+                ('created_at', models.DateField(null=True, verbose_name='\u0414\u0430\u0442\u0430 \u0441\u0442\u0432\u043e\u0440\u0435\u043d\u043d\u044f')),
+                ('modified_at', models.DateField(default=b'', verbose_name='\u0414\u0430\u0442\u0430 \u0437\u043c\u0456\u043d\u0435\u043d\u043d\u044f', blank=True)),
             ],
             options={
+                'verbose_name': '\u041f\u0440\u043e\u0434\u0443\u043a\u0442',
+                'verbose_name_plural': '\u041f\u0440\u043e\u0434\u0443\u043a\u0442\u0438',
             },
-            bases=(models.Model,),
         ),
     ]
-
--- products/models.py
+++ products/models.py
@@ -6,39 +6,37 @@ class Product(models.Model):
     """Product Model"""
 
 
-class Meta(object):
-    verbose_name = u"Продукт"
-    verbose_name_plural = u"Продукти"
+    class Meta(object):
+        verbose_name = u"Продукт"
+        verbose_name_plural = u"Продукти"
 
 
-first_name = models.CharField(
-    max_length=256,
-    blank=False,
-    verbose_name=u"Назва")
+    first_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Назва")
 
-slug = models.SlugField(
-    max_length=50,
-    blank=False,
-    verbose_name=u"Коротка назва-мітка")
-description = models.CharField(
-    max_length=256,
-    blank=False,
-    verbose_name=u"Опис"
-)
-price = models.DecimalField(
-    max_digits=5,
-    decimal_places=2,
-    blank=False,
-    verbose_name=u"Ціна")
-created_at = models.DateField(
-    blank=False,
-    verbose_name=u"Дата створення",
-    null=True)
-modified_at = models.DateField(
-    blank=True,
-    verbose_name=u"Дата змінення",
-    default='')
+    slug = models.SlugField(
+        max_length=50,
+        blank=False,
+        verbose_name=u"Коротка назва-мітка")
+    description = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Опис"
+    )
+    price = models.FloatField(
+        blank=False,
+        verbose_name=u"Ціна")
+    created_at = models.DateField(
+        blank=False,
+        verbose_name=u"Дата створення",
+        null=True)
+    modified_at = models.DateField(
+        blank=True,
+        verbose_name=u"Дата змінення",
+        default='')
 
 
-def __unicode__(self):
-    return u"%s %s" % (self.first_name, self.slug)
+    def __unicode__(self):
+        return u"%s %s" % (self.first_name, self.slug)
