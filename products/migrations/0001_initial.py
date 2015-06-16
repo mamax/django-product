2c9fd1baff453016d47fcfc4f5deab70d5291016
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-06-16 07:19:31 +0300

    add initial models to migration

---
+++ products/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('slug', models.CharField(max_length=256,verbose_name='\u041f\u0440\u0456\u0437\u0432\u0438\u0449\u0435')),
+                ('description', models.CharField(max_length=256,verbose_name='\u041f\u0440\u0456\u0437\u0432\u0438\u0449\u0435')),
+                ('price', models.DecimalField(max_digits=5, decimal_places=2, verbose_name='\u0411\u0456\u043b\u0435\u0442')),
+                ('created_at', models.DateField(verbose_name='\u0414\u0430\u0442\u0430\u043d\u0430\u0440\u043e\u0434\u0436\u0435\u043d\u043d\u044f')),
+                ('modified_at', models.DateField(verbose_name='\u0414\u0430\u0442\u0430\u043d\u0430\u0440\u043e\u0434\u0436\u0435\u043d\u043d\u044f', blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
+
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+import datetime
 
 # Create your models here.
 class Product(models.Model):
@@ -15,23 +16,19 @@ slug = models.SlugField(
     verbose_name=u"Коротка назва-мітка")
 description = models.CharField(
     max_length=256,
-    blank=True,
-    verbose_name=u"Опис",
-    default='')
+    blank=False,
+    verbose_name=u"Опис"
+    )
 price = models.DecimalField(
     max_digits=5,
     decimal_places=2,
     blank=False,
     verbose_name=u"Ціна")
 created_at = models.DateField(
-    auto_now=False,
-    auto_now_add=True,
     blank=False,
     verbose_name=u"Дата створення",
     null=True)
 modified_at = models.DateField(
-    auto_now=True,
-    auto_now_add=False,
     blank=True,
     verbose_name=u"Дата змінення",
     default='')
