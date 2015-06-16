85ca2add0094e05cd9f05b890148162c45ee3eb4
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-06-16 07:21:13 +0300

    add initial models to migration

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
             name='Product',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=256,verbose_name='\u041f\u0440\u0456\u0437\u0432\u0438\u0449\u0435')),
                 ('slug', models.CharField(max_length=256,verbose_name='\u041f\u0440\u0456\u0437\u0432\u0438\u0449\u0435')),
                 ('description', models.CharField(max_length=256,verbose_name='\u041f\u0440\u0456\u0437\u0432\u0438\u0449\u0435')),
                 ('price', models.DecimalField(max_digits=5, decimal_places=2, verbose_name='\u0411\u0456\u043b\u0435\u0442')),
